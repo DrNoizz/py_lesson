b8ca823c7dc4de639543438ef23f224c863fd62b
DrNoizz <noizzpost@gmail.com>
2023-11-16 21:48:26 +1000

    work3

--- learn/Excel отчёт/pd.py
+++ learn/Excel отчёт/pd.py
@@ -9,4 +9,5 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 pd.options.display.expand_frame_repr = False
 
-df.sort_values(by = ["ОБЪЕКТ", "КОНТР СРОК"])
+df.sort_values(by = ["ОБЪЕКТ", "КОНТР СРОК"])  # Если нужно сортировать по убыванию: df.sort_values(by = ["ОБЪЕКТ", "КОНТР СРОК"], ascending = False),
+# если по первому столбцу по возрастанию, а по второму - по убыванию: ascending = [False, True]
