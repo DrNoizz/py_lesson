1d23efa7ab84fdb64cb12ff0a24e87c59d59baf8
DrNoizz <noizzpost@gmail.com>
2023-11-16 19:19:13 +1000

    commit6

--- learn/Excel отчёт/test.py
+++ learn/Excel отчёт/test.py
@@ -1,4 +1,5 @@
 import openpyxl, pytz
+from openpyxl.styles import Font, PatternFill
 from datetime import datetime, time
 
 book = openpyxl.open("C:/Users/Noizz/Desktop/learn/Excel отчёт/Сводка по объектам SM.xlsx", read_only=False)
@@ -11,14 +12,14 @@ for row in range(2, sheet.max_row + 1):  # Начинаем с 2-ой строк
     group = sheet[row][8].value
     employee = sheet[row][9].value
     date = control_time
-    date = date.replace(tzinfo=pytz.timezone('Europe/Moscow'))
+    date = date.replace(tzinfo=pytz.timezone('Europe/Moscow')) # replace для приведения обеих дат к одному виду, иначе ошибка "can't compare offset-naive and offset-aware datetimes"
     format = "%y/%m/%d %H:%M:%S"
 
     dt = datetime.now(pytz.timezone('Europe/Moscow'))
     dt = dt.replace(microsecond=0)
 
-    # print(type(dt), type(date))
     if date < dt:
-        print(zno, object, control_time, group, employee)
+        print(zno, object, control_time, group, employee, sep='  |-----|  ')
+
 
 # date = date.replace(tzinfo=pytz.timezone('Europe/Moscow'))
